085ca1df81405d5a88124bd230d8063ee9156b68
Margrethe Moxnes <margrethe@moxnesdesign.no>
2016-03-09 16:25:22 -0800

    Commented out JS code

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -8,61 +8,39 @@ $(document).ready(function(){
 
 	var nav = $('.navbar-header');
 
-	console.log(sidebar.position().top+' og '+nav.position().top);
 
-	var items = $('#sidebar .item').length;
+	var items = $('.types');
 
 	var distance = 250;
+	var lastScrollTop = 0;
 
+	
 
+		// var sidebarTop = $('#sidebar').position().top;
+		// var lastScrollTop = 0;
+		// win.scroll(function () {
+		// 	var st = $(this).scrollTop();
+		// 		if( sidebarTop > 0){
+		// 			if (st > lastScrollTop) {
+		// 			    $('#sidebar').animate({top: '-=5'}, 5);
+		// 			    console.log(sidebarTop);
+		// 			} else {
+		// 			    $('#sidebar').animate({top: '+=5'}, 5);
+		// 			}
+		// 		}
+		// 	lastScrollTop = st;
+		// })
 
-	win.scroll(function() {
-		for( x = 0; x < items; x++){
-			var menuItem = $('item-'+x).position();
-
-			//.position().top-distance;
-			console.log(menuItem);
-			if(menuItem < win.scrollTop()){
-				// $('#sidebar a').removeClass('active');
-				// $(this).find('a').addClass('active');
-				console.log('Found it');
-			}
-		}
-	});
 
 
 	// Scroll on click
-	jQuery('#sidebar a').on('click', function(){
+	$('#sidebar a').on('click', function(){
 		event.preventDefault();
-		var menuItem = jQuery(this);
-		var href = jQuery(menuItem).attr('href');
-		//var target = href.substr(1);
-		jQuery('#sidebar a').removeClass('active');
+		var menuItem = $(this);
+		var href = $(menuItem).attr('href');
+		$('#sidebar a').removeClass('active');
 		menuItem.addClass('active');
-		jQuery('html, body').animate({scrollTop: $(href).offset().top-distance}, 500, 'linear');
+		$('html, body').animate({scrollTop: $(href).offset().top-distance}, 500, 'linear');
 	});
 
-	// win.scroll(function(){
-	// 	if(kontakt < win.scrollTop()){
-	// 		jQuery('.nav.nav-pills li').removeClass('active');
-	// 		jQuery('.nav.nav-pills li.kontakt').addClass('active');
-	// 	}
-	// 	else if(bb < win.scrollTop()){
-	// 		jQuery('.nav.nav-pills li').removeClass('active');
-	// 		jQuery('.nav.nav-pills li.bestille-boker').addClass('active');
-			
-	// 	}
-	// 	else if(forfattere < win.scrollTop()){
-	// 		jQuery('.nav.nav-pills li').removeClass('active');
-	// 		jQuery('.nav.nav-pills li.forfattere').addClass('active');
-	// 	}
-	// 	else if(boker < win.scrollTop()){
-	// 		jQuery('.nav.nav-pills li').removeClass('active');
-	// 		jQuery('.nav.nav-pills li.boker').addClass('active');
-	// 	}
-	// 	else if(hjem < win.scrollTop()) {
-	// 		jQuery('.nav.nav-pills li').removeClass('active');
-	// 		jQuery('.nav.nav-pills li.home').addClass('active');
-	// 	}
-	// });
 });
